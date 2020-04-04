b88f26941ba7d7bb9ea221b8ca1f1803a11b420c
jatasya <vikas.cta89@gmail.com>
2020-04-04 22:36:25 +0530

    basic complete instagram post example with divider repeat container

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,50 @@ class _HomePageState extends State<HomePage> {
                     Divider(color: Colors.black,)
                   ],
                 ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 25,
+                          child: Image.network('https://image.tmdb.org/t/p/original/tUpNOTyHtamAt8GlEXsBKvbaDqu.jpg',
+                            height: 40, fit: BoxFit.cover,
+                          ),
+
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Queen', style: TextStyle(fontWeight: FontWeight.bold, ),),
+                            Text('South Korea')
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top:10),
+                      child: Image.network('https://static2.srcdn.com/wordpress/wp-content/uploads/2018/02/Jake-Sully-and-Neytiti-in-Avatar.jpg'),
+
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top:10),
+                      child: Row(
+
+                        children: <Widget>[
+                          Icon(Icons.thumb_up),
+                          SizedBox(width: 10,),
+                          Icon(Icons.comment),
+                          SizedBox(width: 10,),
+                          Icon(Icons.share)
+                        ],
+                      ),
+                    ),
+                    Divider(color: Colors.black,)
+                  ],
+                ),
               )
             ],
           ),
